91661c2f3e6b7476b37d0231655e0d7d7ccadb30
PashaIQinf <opa2005@yandex.ru>
2026-06-19 20:52:08 +1000

    Исправление GitHub Actions (CI/CD) на pytest coverage.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 # 1. Создаем асинхронный движок (Engine)
+if settings.TESTING and settings.TEST_DATABASE_URL:
+    DATABASE_URL = settings.TEST_DATABASE_URL
+else:
+    DATABASE_URL = settings.DATABASE_URL
+
 engine = create_async_engine(settings.DATABASE_URL, echo=True) # echo=True покажет SQL-запросы в терминале
+
 
 # 2. Фабрика сессий (будем использовать её в FastAPI для каждого запроса)
 async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession,  expire_on_commit=False)
